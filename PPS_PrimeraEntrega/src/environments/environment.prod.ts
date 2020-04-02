4070c950c8fdc259df581f0fd116abee75c95f16
laloo96 <ohmycaio@gmail.com>
2020-04-02 19:02:31 -0300

    firebasecofing en enviromentprod

--- PPS_PrimeraEntrega/src/environments/environment.prod.ts
+++ PPS_PrimeraEntrega/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDXfG6D8yFLmtV56PCM1_MzzjcYsH2DcDc",
+    authDomain: "primeraentrega-login.firebaseapp.com",
+    databaseURL: "https://primeraentrega-login.firebaseio.com",
+    projectId: "primeraentrega-login",
+    storageBucket: "primeraentrega-login.appspot.com",
+    messagingSenderId: "224291654953",
+    appId: "1:224291654953:web:70b0742f0f5e528db77b38",
+    measurementId: "G-F2ZTV43SXB"
+  }
 };
